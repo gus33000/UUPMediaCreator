a4ac09b4714b62051690103c59037e145a6865d1
Gustave Monce <gustave.monce@outlook.com>
2021-03-04 08:57:51 +0100

    Bump version to 0.3.0.2

--- src/Applications/UUPDownload/Properties/AssemblyInfo.cs
+++ src/Applications/UUPDownload/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Gustave Monce and Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.3.0.1")]
-[assembly: AssemblyFileVersion("0.3.0.1")]
+[assembly: AssemblyVersion("0.3.0.2")]
+[assembly: AssemblyFileVersion("0.3.0.2")]
