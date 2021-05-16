0e74418a53f5278e3ee0e76fcba8febdca47fb02
Gustave Monce <gustave.monce@outlook.com>
2021-05-16 09:48:10 +0200

    Add another workaround for Dism failing while applying the unattend file

--- src/MediaCreationLib.DismOperations.NET/DismOperations.cs
+++ src/MediaCreationLib.DismOperations.NET/DismOperations.cs
@@ -128,44 +128,56 @@ namespace MediaCreationLib.Dism
 
         public static void ApplyUnattend(string ospath, string unattendpath)
         {
-            bool ok = false;
-            while (!ok)
+            int counter = 0;
+            //
+            // Initialize DISM log
+            //
+            tryagain:
+            string tempLog = Path.GetTempFileName();
+            DismApi.Initialize(DismLogLevel.LogErrorsWarningsInfo, tempLog);
+            DismSession session = DismApi.OpenOfflineSession(ospath);
+
+            try
             {
                 try
                 {
-                    //
-                    // Initialize DISM log
-                    //
-                    string tempLog = Path.GetTempFileName();
-                    DismApi.Initialize(DismLogLevel.LogErrorsWarningsInfo, tempLog);
-
-                    var session = DismApi.OpenOfflineSession(ospath);
-
-                    try
-                    {
-                        DismApi.ApplyUnattend(session, unattendpath, true);
-                    }
-                    finally //(Exception ex)
-                    {
-                    }
-
-                    //
-                    // Clean DISM
-                    //
-                    DismApi.CloseSession(session);
-                    DismApi.Shutdown();
-                    File.Delete(tempLog);
-
-                    ok = true;
+                    DismApi.ApplyUnattend(session, unattendpath, true);
                 }
-                catch (Exception ex)
+                finally //(Exception ex)
                 {
-                    Console.WriteLine("Failed applying unattend, retrying in one second...");
-                    Console.WriteLine(ex.ToString());
-                    ok = false;
-                    Thread.Sleep(1000);
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed applying unattend, retrying in one second...");
+                Console.WriteLine("Failed attempt #" + counter);
+                Console.WriteLine(ex.ToString());
+                //
+                // Clean DISM
+                //
+                DismApi.CloseSession(session);
+                try
+                {
+                    DismApi.Shutdown();
+                }
+                catch { }
+                if (counter < 3)
+                {
+                    Thread.Sleep(1000);
+                    counter++;
+                    goto tryagain;
+                }
+            }
+
+            //
+            // Clean DISM
+            //
+            try
+            {
+                DismApi.CloseSession(session);
+            }
+            catch { }
+            DismApi.Shutdown();
         }
 
         public static void SetProductKey(string ospath, string productkey)
@@ -215,6 +227,7 @@ namespace MediaCreationLib.Dism
             }
             catch (Exception ex)
             {
+                Console.WriteLine("Failed setting edition, retrying in one second...");
                 Console.WriteLine("Failed attempt #" + counter);
                 Console.WriteLine(ex.ToString());
                 //
@@ -228,6 +241,7 @@ namespace MediaCreationLib.Dism
                 catch { }
                 if (counter < 3)
                 {
+                    Thread.Sleep(1000);
                     counter++;
                     goto tryagain;
                 }
