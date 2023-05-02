6a3653ae20683fd374893c49b30af2914597d4cf
Gustave Monce <gustave.monce@outlook.com>
2023-05-02 15:50:09 +0200

    Acknowledge the existence of Windows on ARM

---
+++ publish_win_arm64.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+msbuild /m /t:restore,cli\uupmediaconverter:publish,cli\uupdownload:publish,cli\uupmediaconverterdismbroker:publish /p:Platform=arm64 /p:RuntimeIdentifier=win-arm64 /p:PublishDir=%CD%\publish\artifacts\win-arm64\CLI /p:PublishSingleFile=true /p:PublishTrimmed=false /p:Configuration=Release UUPMediaCreator.sln
