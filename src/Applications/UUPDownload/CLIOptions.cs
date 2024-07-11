c0bf7220724d1a8f8d7733f76ce6f4c50266aaf8
AnubyteCode <167210503+AnubyteCode@users.noreply.github.com>
2024-07-11 11:22:04 -0600

    Cleaned the updated CLIOptions.cs
    and reverted the default verb

--- src/Applications/UUPDownload/CLIOptions.cs
+++ src/Applications/UUPDownload/CLIOptions.cs
@@ -24,47 +24,42 @@ using UnifiedUpdatePlatform.Services.WindowsUpdate;
 
 namespace UUPDownload
 {
-    [Verb("request-download", isDefault: false, HelpText = "Request a download from zero using a number of different request parameters. Use 'help' for more features.")]
+    [Verb("request-download", isDefault: true, HelpText = "Request a download from zero using a different parameters. Use 'help' to see more options.")]
     internal class DownloadRequestOptions
     {
         [Option('s', "reporting-sku", HelpText = "The sku to report to the Windows Update servers." +
-            "Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = true, Default = "")]
+            
+  Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = true, Default = "Professional")]
         public OSSkuId ReportingSku
         {
             get; set;
         }
 
-        [Option('v', "reporting-version", HelpText = "The version to report to the Windows Update servers." +
-            "Example: 10.0.21262.1000.rs_prerelease.201113-1441, 10.0.25832.1", Required = true)]
+        [Option('v', "reporting-version", HelpText = "The version to report to the Windows Update servers.\n  Example: 10.0.21262.1000.rs_prerelease.201113-1441, 10.0.25832.1", Required = true)]
         public string ReportingVersion
         {
             get; set;
         }
 
-        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers." +
-            "Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb," +
-            "woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
+        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers.\n  Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb, woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
         public MachineType MachineType
         {
             get; set;
         }
 
-        [Option('r', "flight-ring", HelpText = "The ring to report to the Windows Update servers." +
-            "Example: Retail, External, Internal", Required = true)]
+        [Option('r', "flight-ring", HelpText = "The ring to report to the Windows Update servers.\n  Example: Retail, External, Internal", Required = true)]
         public string FlightRing
         {
             get; set;
         }
 
-        [Option('c', "current-branch", HelpText = "The branch to report to the Windows Update servers." +
-            "Example: rs5_release, 19h1_release (TI), co_refresh, ni_release, ge_prerelease, ge_release", Required = true)]
+        [Option('c', "current-branch", HelpText = "The branch to report to the Windows Update servers.\n  Example: rs3_release, 19h1_release (TI), co_refresh, ni_release, ge_prerelease, ge_release", Required = true)]
         public string CurrentBranch
         {
             get; set;
         }
 
-        [Option('b', "flighting-branch-name", HelpText = "The flighting branch name to report to the Windows Update servers." +
-            "Example: Retail, CanaryChannel, Dev, Beta, ReleasePreview (RP), WindowsInsiderSlow/Fast (WIS/WIF)", Required = false, Default = "")]
+        [Option('b', "flighting-branch-name", HelpText = "The flighting branch name to report to the Windows Update servers.\n  Example: Retail, CanaryChannel, Dev, Beta, ReleasePreview (RP), WindowsInsiderSlow/Fast (WIS/WIF)", Required = false, Default = "")]
         public string FlightingBranchName
         {
             get; set;
@@ -76,8 +71,7 @@ namespace UUPDownload
             get; set;
         }
 
-        [Option('a', "branch-readiness-level", HelpText = "The branch readiness level to report to the Windows Update servers." +
-            "Example: CB, CBB, LTSB, LTSC", Required = false, Default = "CB")]
+        [Option('a', "branch-readiness-level", HelpText = "The branch readiness level to report to the Windows Update servers.\n  Example: CB, CBB, LTSB, LTSC", Required = false, Default = "CB")]
         public string BranchReadinessLevel
         {
             get; set;
@@ -89,29 +83,27 @@ namespace UUPDownload
             get; set;
         }
 
-        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything." +
-            "Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.'", Required = false, Default = "")]
+        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything.\n  Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.'", Required = false, Default = "")]
         public string Edition
         {
             get; set;
         }
 
-        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything." +
-            "Example: en-US, de-DE, zh-CN", Required = false, Default = "")]
+        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything.\n  Example: en-US, de-DE, zh-CN", Required = false, Default = "")]
         public string Language
         {
             get; set;
         }
 
         [Option('z', "releasetype", HelpText = "The release type to report to the Windows Update servers." +
-            "Example: Production", Required = false, Default = "Production")]
+            
+  Example: Production", Required = false, Default = "Production")]
         public string ReleaseType
         {
             get; set;
         }
 
-        [Option('n', "contenttype", HelpText = "The content type to report to the Windows Update servers." +
-            "Example: Mainline, Custom", Required = false, Default = "Mainline")]
+        [Option('n', "contenttype", HelpText = "The content type to report to the Windows Update servers.\n  Example: Mainline, Custom", Required = false, Default = "Mainline")]
         public string ContentType
         {
             get; set;
@@ -123,8 +115,7 @@ namespace UUPDownload
             get; set;
         }
 
-        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens." +
-            "(If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
+        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens.\n  (If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
         public string Password
         {
             get; set;
@@ -134,16 +125,13 @@ namespace UUPDownload
     [Verb("replay-download", isDefault: false, HelpText = "Replay a download from zero using a *.uupmcreplay file.")]
     internal class DownloadReplayOptions
     {
-        [Option('r', "replay-metadata", HelpText = "The path to a *.uupmcreplay file to replay an older update and resume the download process." +
-            @"Example: D:\20236.1005.uupmcreplay", Required = true)]
+        [Option('r', "replay-metadata", HelpText = "The path to a *.uupmcreplay file to replay an older update and resume the download process.\n  Example: D:\\20236.1005.uupmcreplay", Required = true)]
         public string ReplayMetadata
         {
             get; set;
         }
 
-        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers." +
-            "Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb," +
-            "woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
+        [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers.\n  Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb,\n woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
         public MachineType MachineType
         {
             get; set;
@@ -155,22 +143,19 @@ namespace UUPDownload
             get; set;
         }
 
-        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything." +
-            "Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = false, Default = "")]
+        [Option('e', "edition", HelpText = "The edition to get. Must be used with the language parameter. Omit either of these to download everything.\n  Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = false, Default = "")]
         public string Edition
         {
             get; set;
         }
 
-        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything." +
-            "Example: en-US, de-DE, zh-CN", Required = false, Default = "")]
+        [Option('l', "language", HelpText = "The language to get. Must be used with the edition parameter. Omit either of these to download everything.\n  Example: en-US, de-DE, zh-CN", Required = false, Default = "")]
         public string Language
         {
             get; set;
         }
 
-        [Option('f', "fixup", HelpText = "Applies a fixup to files in output folder." +
-            "Example: Appx", Required = false)]
+        [Option('f', "fixup", HelpText = "Applies a fixup to files in output folder.\n  Example: Appx", Required = false)]
         public Fixup? Fixup
         {
             get; set;
@@ -192,16 +177,15 @@ namespace UUPDownload
     [Verb("get-builds", isDefault: false, HelpText = "Get builds in all rings matching the request type")]
     internal class GetBuildsOptions
     {
-        [Option('s', "reporting-sku", HelpText = "The sku to report to the Windows Update servers. " +
-            "Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = true, Default = "")]
+        [Option('s', "reporting-sku", HelpText = "The sku to report to the Windows Update servers.\n  Example: Professional, EnterpriseG. Enter 'type editions.txt' to see a full list of possibilities.' ", Required = true, Default = "")]
         public OSSkuId ReportingSku
         {
             get; set;
         }
 
         [Option('t', "machine-type", HelpText = "The architecture to report to the Windows Update servers." +
-            "Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb," +
-            "woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
+            
+  Example: amd64, arm64, unknown, x86, wcemipsv2, axp, sh3, sh3dsp, sh4, sh5, arm, thumb,\n woa, am33, powerpc, powerpcfp, ia64, mips16, mipsfpu, mipsfpu16, ebc, m32r", Required = true)]
         public MachineType MachineType
         {
             get; set;
@@ -213,14 +197,13 @@ namespace UUPDownload
             get; set;
         }
 
-        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens." +
-            "(If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
+        [Option('p', "password", HelpText = "Password for the Windows Insider account to use to generate authorization tokens.\n (If 2FA, must be generated app password) (Optional)", Required = false, Default = "")]
         public string Password
         {
             get; set;
         }
 
-        [Option('q', "preview-targeting-attribute", HelpText = "The name of the set of targeting attributes to use. (Optional, Preview)", Required = false, Default = "")]
+        [Option('q', "preview-targeting-attribute", HelpText = "The name of the set of targeting attributes to use.", Required = false, Default = "")]
         public string TargetingAttribute
         {
             get; set;
