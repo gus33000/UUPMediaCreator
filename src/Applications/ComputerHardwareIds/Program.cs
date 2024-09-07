36b2e34b4d166aab8167b5a3d79caf06db3b4d9b
Gustave Monce <gustave.monce@outlook.com>
2024-09-07 20:42:40 +0200

    Cleanup CHID tool code

--- src/Applications/ComputerHardwareIds/Program.cs
+++ src/Applications/ComputerHardwareIds/Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Management.Infrastructure.Options;
-using Microsoft.Management.Infrastructure;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using UnifiedUpdatePlatform.Services.WindowsUpdate.Targeting;
 
 namespace ComputerHardwareIds
@@ -12,31 +10,33 @@ namespace ComputerHardwareIds
             Process currentProcess = Process.GetCurrentProcess();
             ProcessModule MainModule = currentProcess.MainModule!;
 
-            using DComSessionOptions dcomSessionOptions = new DComSessionOptions();
-            using CimSession cimSession = CimSession.Create("localhost", dcomSessionOptions);
+            (string Manufacturer,
+                   string Family,
+                   string ProductName,
+                   string SKUNumber,
+                   string BIOSVendor,
+                   string BaseboardManufacturer,
+                   string BaseboardProduct,
+                   ushort EnclosureType,
+                   string BIOSVersion,
+                   byte BIOSMajorRelease,
+                   byte BIOSMinorRelease) = ComputerInformationFetcher.FetchComputerInformation();
 
-            CimInstance result = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT * FROM Win32_BIOS").Single();
-
-            string BIOSVendor = (string)result.CimInstanceProperties["Manufacturer"].Value;
-            string BIOSVersionString = (string)result.CimInstanceProperties["SMBIOSBIOSVersion"].Value;
-            byte SystemBIOSMajorRelease = (byte)result.CimInstanceProperties["SystemBiosMajorVersion"].Value;
-            byte SystemBIOSMinorRelease = (byte)result.CimInstanceProperties["SystemBiosMinorVersion"].Value;
-
-            result = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT * FROM Win32_ComputerSystem").Single();
-
-            string SystemManufacturer = (string)result.CimInstanceProperties["Manufacturer"].Value;
-            string SystemFamily = (string)result.CimInstanceProperties["SystemFamily"].Value;
-            string SystemProductName = (string)result.CimInstanceProperties["Model"].Value;
-            string SKUNumber = (string)result.CimInstanceProperties["SystemSKUNumber"].Value;
-
-            result = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT * FROM Win32_SystemEnclosure").Single();
-
-            ushort SystemEnclosureorChassisType = ((ushort[])result.CimInstanceProperties["ChassisTypes"].Value)[0];
-
-            result = cimSession.QueryInstances(@"root\cimv2", "WQL", "SELECT * FROM Win32_BaseBoard").Single();
-
-            string BaseboardManufacturer = (string)result.CimInstanceProperties["Manufacturer"].Value;
-            string BaseboardProductName = (string)result.CimInstanceProperties["Product"].Value;
+            string HardwareId1 = ComputerHardwareID.GenerateHardwareId1(Manufacturer, Family, ProductName, SKUNumber, BIOSVendor, BIOSVersion, BIOSMajorRelease.ToString("X").ToLower(), BIOSMinorRelease.ToString("X").ToLower());
+            string HardwareId2 = ComputerHardwareID.GenerateHardwareId2(Manufacturer, Family, ProductName, BIOSVendor, BIOSVersion, BIOSMajorRelease.ToString("X").ToLower(), BIOSMinorRelease.ToString("X").ToLower());
+            string HardwareId3 = ComputerHardwareID.GenerateHardwareId3(Manufacturer, ProductName, BIOSVendor, BIOSVersion, BIOSMajorRelease.ToString("X").ToLower(), BIOSMinorRelease.ToString("X").ToLower());
+            string HardwareId4 = ComputerHardwareID.GenerateHardwareId4(Manufacturer, Family, ProductName, SKUNumber, BaseboardManufacturer, BaseboardProduct);
+            string HardwareId5 = ComputerHardwareID.GenerateHardwareId5(Manufacturer, Family, ProductName, SKUNumber);
+            string HardwareId6 = ComputerHardwareID.GenerateHardwareId6(Manufacturer, Family, ProductName);
+            string HardwareId7 = ComputerHardwareID.GenerateHardwareId7(Manufacturer, SKUNumber, BaseboardManufacturer, BaseboardProduct);
+            string HardwareId8 = ComputerHardwareID.GenerateHardwareId8(Manufacturer, SKUNumber);
+            string HardwareId9 = ComputerHardwareID.GenerateHardwareId9(Manufacturer, ProductName, BaseboardManufacturer, BaseboardProduct);
+            string HardwareId10 = ComputerHardwareID.GenerateHardwareId10(Manufacturer, ProductName);
+            string HardwareId11 = ComputerHardwareID.GenerateHardwareId11(Manufacturer, Family, BaseboardManufacturer, BaseboardProduct);
+            string HardwareId12 = ComputerHardwareID.GenerateHardwareId12(Manufacturer, Family);
+            string HardwareId13 = ComputerHardwareID.GenerateHardwareId13(Manufacturer, EnclosureType.ToString());
+            string HardwareId14 = ComputerHardwareID.GenerateHardwareId14(Manufacturer, BaseboardManufacturer, BaseboardProduct);
+            string HardwareId15 = ComputerHardwareID.GenerateHardwareId15(Manufacturer);
 
             Console.WriteLine("Using the BIOS to gather information");
             Console.WriteLine();
@@ -48,38 +48,38 @@ namespace ComputerHardwareIds
             Console.WriteLine("Computer Information");
             Console.WriteLine("--------------------");
             Console.WriteLine($"BIOS Vendor: {BIOSVendor}");
-            Console.WriteLine($"BIOS Version String: {BIOSVersionString}");
-            Console.WriteLine($"System BIOS Major Release: {SystemBIOSMajorRelease}");
-            Console.WriteLine($"System BIOS Minor Release: {SystemBIOSMinorRelease}");
+            Console.WriteLine($"BIOS Version String: {BIOSVersion}");
+            Console.WriteLine($"System BIOS Major Release: {BIOSMajorRelease}");
+            Console.WriteLine($"System BIOS Minor Release: {BIOSMinorRelease}");
             Console.WriteLine();
-            Console.WriteLine($"System Manufacturer: {SystemManufacturer}");
-            Console.WriteLine($"System Family: {SystemFamily}");
-            Console.WriteLine($"System Product Name: {SystemProductName}");
+            Console.WriteLine($"System Manufacturer: {Manufacturer}");
+            Console.WriteLine($"System Family: {Family}");
+            Console.WriteLine($"System Product Name: {ProductName}");
             Console.WriteLine($"SKU Number: {SKUNumber}");
             Console.WriteLine();
-            Console.WriteLine($"System Enclosure or Chassis Type: {SystemEnclosureorChassisType:X2}h");
+            Console.WriteLine($"System Enclosure or Chassis Type: {EnclosureType:X2}h");
             Console.WriteLine();
             Console.WriteLine($"Baseboard Manufacturer: {BaseboardManufacturer}");
-            Console.WriteLine($"Baseboard Product Name: {BaseboardProductName}");
+            Console.WriteLine($"Baseboard Product Name: {BaseboardProduct}");
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("Hardware IDs");
             Console.WriteLine("------------");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId1(SystemManufacturer, SystemFamily, SystemProductName, SKUNumber, BIOSVendor, BIOSVersionString, SystemBIOSMajorRelease.ToString("X").ToLower(), SystemBIOSMinorRelease.ToString("X").ToLower())}    <- Manufacturer + Family + ProductName + SKUNumber + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId2(SystemManufacturer, SystemFamily, SystemProductName, BIOSVendor, BIOSVersionString, SystemBIOSMajorRelease.ToString("X").ToLower(), SystemBIOSMinorRelease.ToString("X").ToLower())}    <- Manufacturer + Family + ProductName + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId3(SystemManufacturer, SystemProductName, BIOSVendor, BIOSVersionString, SystemBIOSMajorRelease.ToString("X").ToLower(), SystemBIOSMinorRelease.ToString("X").ToLower())}    <- Manufacturer + ProductName + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId4(SystemManufacturer, SystemFamily, SystemProductName, SKUNumber, BaseboardManufacturer, BaseboardProductName)}    <- Manufacturer + Family + ProductName + SKUNumber + Baseboard Manufacturer + Baseboard Product");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId5(SystemManufacturer, SystemFamily, SystemProductName, SKUNumber)}    <- Manufacturer + Family + ProductName + SKUNumber");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId6(SystemManufacturer, SystemFamily, SystemProductName)}    <- Manufacturer + Family + ProductName");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId7(SystemManufacturer, SKUNumber, BaseboardManufacturer, BaseboardProductName)}    <- Manufacturer + SKUNumber + Baseboard Manufacturer + Baseboard Product");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId8(SystemManufacturer, SKUNumber)}    <- Manufacturer + SKUNumber");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId9(SystemManufacturer, SystemProductName, BaseboardManufacturer, BaseboardProductName)}    <- Manufacturer + ProductName + Baseboard Manufacturer + Baseboard Product");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId10(SystemManufacturer, SystemProductName)}    <- Manufacturer + ProductName");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId11(SystemManufacturer, SystemFamily, BaseboardManufacturer, BaseboardProductName)}    <- Manufacturer + Family + Baseboard Manufacturer + Baseboard Product");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId12(SystemManufacturer, SystemFamily)}    <- Manufacturer + Family");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId13(SystemManufacturer, SystemEnclosureorChassisType.ToString())}    <- Manufacturer + Enclosure Type");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId14(SystemManufacturer, BaseboardManufacturer, BaseboardProductName)}    <- Manufacturer + Baseboard Manufacturer + Baseboard Product");
-            Console.WriteLine($"{ComputerHardwareID.GenerateHardwareId15(SystemManufacturer)}    <- Manufacturer");
+            Console.WriteLine($"{HardwareId1}    <- Manufacturer + Family + ProductName + SKUNumber + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
+            Console.WriteLine($"{HardwareId2}    <- Manufacturer + Family + ProductName + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
+            Console.WriteLine($"{HardwareId3}    <- Manufacturer + ProductName + BIOS Vendor + BIOS Version + BIOS Major Release + BIOS Minor Release");
+            Console.WriteLine($"{HardwareId4}    <- Manufacturer + Family + ProductName + SKUNumber + Baseboard Manufacturer + Baseboard Product");
+            Console.WriteLine($"{HardwareId5}    <- Manufacturer + Family + ProductName + SKUNumber");
+            Console.WriteLine($"{HardwareId6}    <- Manufacturer + Family + ProductName");
+            Console.WriteLine($"{HardwareId7}    <- Manufacturer + SKUNumber + Baseboard Manufacturer + Baseboard Product");
+            Console.WriteLine($"{HardwareId8}    <- Manufacturer + SKUNumber");
+            Console.WriteLine($"{HardwareId9}    <- Manufacturer + ProductName + Baseboard Manufacturer + Baseboard Product");
+            Console.WriteLine($"{HardwareId10}    <- Manufacturer + ProductName");
+            Console.WriteLine($"{HardwareId11}    <- Manufacturer + Family + Baseboard Manufacturer + Baseboard Product");
+            Console.WriteLine($"{HardwareId12}    <- Manufacturer + Family");
+            Console.WriteLine($"{HardwareId13}    <- Manufacturer + Enclosure Type");
+            Console.WriteLine($"{HardwareId14}    <- Manufacturer + Baseboard Manufacturer + Baseboard Product");
+            Console.WriteLine($"{HardwareId15}    <- Manufacturer");
         }
     }
 }
